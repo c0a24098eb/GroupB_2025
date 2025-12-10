2b13f8c00ac084f8e5c6b24c211b7d4694ad5f04
c0a242657d <c0a242657d@edu.teu.ac.jp>
2025-12-10 23:44:11 +0900

    検査機HSV更新

--- practice_ws/images/get_color4img.py
+++ practice_ws/images/get_color4img.py
@@ -5,13 +5,13 @@ def get_color(event, x, y, flags, param):
     img=param
     if event == cv2.EVENT_LBUTTONDOWN:
         b, g, r = img[y, x]
-        print(f"座標({x}, {y}) の色：B={b}, G={g}, R={r}")
+        print(f"座標({x}, {y}) 䛾色：色相={b}, 彩度={g}, 明度={r}")
 
 # 画像の読み込み
 img = cv2.imread("./shot_test.png")
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 # ウィンドウを表示してコールバック登録
 cv2.imshow("image", img)
-cv2.setMouseCallback("image", get_color, param=img)
-
+cv2.setMouseCallback("image", get_color, param=hsv_img)
 cv2.waitKey(0)
